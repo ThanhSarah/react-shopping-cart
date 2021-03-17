e198374651112f960cea3ca21ea9361eb198d0f4
thanh sarah <thanhnguyen1407ba@gmail.com>
2021-03-17 10:07:42 +0700

    products component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 //feature 1
-import React from 'react';
+import React from "react";
+import data from "./data.json";
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Componenet {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size:"",
+      sort:"",
+    };
+  }
+  render() {
+    return (
       <div className="grid-container">
-        <header>
-        <a href="/"> React Shopping Card</a>
-        </header>
-        <main>
-          Product List
-        </main>
-        <footer>
-          All right is reseved
-        </footer>
-      </div>
+      <header>
+      <a href="/"> React Shopping Card</a>
+      </header>
+      <main>
+        <div className="content">
+          <div className="main">
+            Products
+          </div>
+          <div className="sidebar">
+            Cart items
+          </div>
+        </div>
+        Product List
+      </main>
+      <footer>
+        All right is reseved
+      </footer>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
